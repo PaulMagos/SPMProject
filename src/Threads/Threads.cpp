2f0aba250e4eb9f3e29ec1a854b15755b193ed98
PaulMagos <paulmagos@icloud.com>
2023-08-23 13:32:44 +0200

    Udpate to Writing MEthod  14x achieved

--- src/Threads/Threads.cpp
+++ src/Threads/Threads.cpp
@@ -23,9 +23,9 @@ using namespace std;
 #define OPT_LIST "hi:p:t:"
 
 void writeToFile(const string& bits, const string& encodedFile, int numThreads);
+void writeToFile2(vector<string>* bits, const string& encodedFile, int numThreads);
 vector<int> readFrequencies(ifstream* myFile, int numThreads, uintmax_t len, vector<string>* file);
-string createOutput(vector<string>* myFile, const map<int, string>& myMap, int numThreads);
-
+void createOutput(vector<string>* myFile, const map<int, string>& myMap, int numThreads);
 int main(int argc, char* argv[])
 {
     char option;
@@ -71,15 +71,23 @@ int main(int argc, char* argv[])
         }
         {
             utimer t("Create output");
-            output = createOutput(&file, myMap, numThreads);
+            createOutput(&file, myMap, numThreads);
         }
+//        {
+//            utimer t("Concatenate");
+//            for (auto & i : file) output.append(i);
+//            output.append(string(8-(output.size()%8), '0'));
+//        }
         {
             utimer t("Write to file");
-            writeToFile(output, encodedFile, numThreads);
+//            writeToFile(output, encodedFile, numThreads);
+            writeToFile2(&file, encodedFile, numThreads);
         }
     }
     return 0;
 }
+
+
 
 void calcChar(ifstream* myFile, string* myString, int i, uintmax_t size, uintmax_t size1, mutex* readFileMutex, mutex* writeAsciiMutex, vector<int>* ascii, vector<int>* uAscii){
     {
@@ -135,22 +143,13 @@ void toBits(map<int, string> myMap, string* line){
     *line = bits;
 }
 
-string createOutput(vector<string>* file, const map<int, string>& myMap, int numThreads) {
+void createOutput(vector<string>* file, const map<int, string>& myMap, int numThreads) {
     vector<std::thread> threads;
-    threads.reserve(numThreads);
     for (int i = 0; i < numThreads; i++)
         threads.emplace_back([myMap, capture0 = &(*file)[i]] { return toBits(myMap, capture0); });
-    string output;
     for (int i = 0; i < numThreads; i++) {
         threads[i].join();
     }
-    for (auto & i : *file) {
-        output.append(i);
-    }
-    while (output.size()%8 != 0){
-        output.append("0");
-    }
-    return output;
 }
 
 void writeToFile(const string& bits, const string& encodedFile, int numThreads){
@@ -158,7 +157,7 @@ void writeToFile(const string& bits, const string& encodedFile, int numThreads){
     vector<string> output(numThreads);
     mutex fileMutex;
     vector<std::thread> threads;
-    uintmax_t Start = (((bits.size() - (bits.size() % 8)) / 8) / numThreads + 1) * 8;
+    uintmax_t Start = (((bits.size() - (bits.size() % 8))) / numThreads + 1);
     uintmax_t chunkSize = Start;
     for (int i = 0; i < numThreads; i++) {
         chunkSize += (i==numThreads-1) ? bits.size() - ((i+1)*Start) : 0;
@@ -177,6 +176,50 @@ void writeToFile(const string& bits, const string& encodedFile, int numThreads){
                 outputFile.write(output.c_str(), output.size());
             }
         });
+    }
+    for (int i = 0; i < numThreads; i++) {
+        threads[i].join();
+    }
+}
+
+void writeToFile2(vector<string>* bits, const string& encodedFile, int numThreads){
+    ofstream outputFile(encodedFile, ios::binary | ios::out);
+    vector<string> output(numThreads);
+    mutex fileMutex;
+    vector<std::thread> threads;
+    uintmax_t Start, writePos = 0;
+    uintmax_t End = 0;
+    string line;
+    for (int i = 0; i < numThreads; i++) {
+        Start = End;
+        End = 8 - ((*bits)[i].size()-Start)%8;
+        if (i == numThreads-1)
+            (*bits)[i] += (string(((*bits)[i].size()-Start)%8, '0'));
+        threads.emplace_back([&bits, Start, End, pos=i, writePos, &outputFile, &fileMutex]() {
+            string output;
+            uint8_t value = 0;
+            int i;
+            for (i = Start; i < (*bits)[pos].size(); i+=8) {
+                for (uint8_t n = 0; n < 8; n++)
+                    value = ((*bits)[pos][i+n]=='1') | value << 1;
+                output.append((char*) (&value), 1);
+                value = 0;
+            }
+            if (pos != (*bits).size()-1) {
+                for (uint8_t n = 0; n < 8-End; n++)
+                    value = ((*bits)[pos][i-8+n]=='1') | value << 1;
+                for (i = 0; i < End; i++){
+                    value = ((*bits)[pos+1][i]=='1') | value << 1;
+                }
+                output.append((char*) (&value), 1);
+            }
+            {
+                unique_lock<mutex> lock(fileMutex);
+                outputFile.seekp(writePos/8);
+                outputFile.write(output.c_str(), output.size());
+            }
+        });
+        writePos += (((*bits)[i].size()-Start)+End);
     }
     for (int i = 0; i < numThreads; i++) {
         threads[i].join();
