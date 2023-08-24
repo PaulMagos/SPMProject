fb5950b2a8c71c0857ac24664029bc8bc2021c69
PaulMagos <p.magos@studenti.unipi.com>
2023-08-24 12:20:16 +0200

    FasfFlow Work

--- src/FF/FFFarm.cpp
+++ src/FF/FFFarm.cpp
@@ -4,10 +4,12 @@
 #include "./fastflow/ff/ff.hpp"
 #include <ff/parallel_for.hpp>
 #include "../utils/utimer.cpp"
+#include "../utils/Node.h"
 #include "../utils/utils.cpp"
 #include <iostream>
 #include <fstream>
 #include <utility>
+#include <thread>
 #include <map>
 #include <vector>
 #include <mutex>
@@ -26,7 +28,6 @@
 
 using namespace std;
 using namespace ff;
-
 int NUM_OF_THREADS = 4;
 
 int main(int argc, char* argv[])
@@ -69,38 +70,54 @@ int main(int argc, char* argv[])
     mutex readFileMutex;
     mutex writeAsciiMutex;
 
+    {
+        utimer timer("Total");
+        ffTime(START_TIME);
 
-    ffTime(START_TIME);
+        /* -----------------        FREQ        ----------------- */
+        FF_PARFOR_BEGIN(test1, i, 0, NUM_OF_THREADS, 1, 1, NUM_OF_THREADS) {
+            calcChar(&in, &file[i], i, NUM_OF_THREADS, fileSize, &readFileMutex, &writeAsciiMutex, &ascii);
+        }FF_PARFOR_END(test1);
+        ffTime(STOP_TIME);
+        printf("Time =%g\n", ffTime(GET_TIME));
 
-    /* -----------------        FREQ        ----------------- */
-    FF_PARFOR_BEGIN(test1, i, 0, NUM_OF_THREADS, 1, 2, NUM_OF_THREADS) {
-        calcChar(&in, &file[i], i, NUM_OF_THREADS, fileSize, &readFileMutex, &writeAsciiMutex, &ascii);
-    } FF_PARFOR_END(test1);
-    ffTime(STOP_TIME);
-    printf("Time =%g\n", ffTime(GET_TIME));
+        /* -----------------        HUFFMAN        ----------------- */
+        {
+            utimer timr("Huffman");
+            Node::createMap(Node::buildTree(ascii), &myMap);
+        }
+        /* -----------------        MAP        ----------------- */
+        ffTime(START_TIME);
+        FF_PARFOR_BEGIN(test2, i, 0, NUM_OF_THREADS, 1, 1, NUM_OF_THREADS) {
+            toBits(myMap, &file[i]);
+        }FF_PARFOR_END(test2);
+        ffTime(STOP_TIME);
+        printf("Time =%g\n", ffTime(GET_TIME));
 
-    /* -----------------        MAP        ----------------- */
-    ffTime(START_TIME);
-    FF_PARFOR_BEGIN(test2, i, 0, NUM_OF_THREADS, 1, 2, NUM_OF_THREADS) {
-        toBits(myMap, &file[i]);
-    } FF_PARFOR_END(test2);
-    ffTime(STOP_TIME);
-    printf("Time =%g\n", ffTime(GET_TIME));
-
-    /* -----------------        OUTPUT        ----------------- */
-    ffTime(START_TIME);
-    uintmax_t writePos = 0;
-    uintmax_t Start, End = 0;
-    ofstream outputFile(encodedFile, ios::binary | ios::out);
-    mutex fileMutex;
-    FF_PARFOR_BEGIN(test3, i, 0, NUM_OF_THREADS, 1, 2, NUM_OF_THREADS) {
-        Start = End;
-        End = 8 - ((file)[i].size()-Start)%8;
-        wWrite(Start, End, &file, i, writePos, &outputFile, &fileMutex);
-        writePos += ((file[i].size()-Start)+End);
-    } FF_PARFOR_END(test3);
-    ffTime(STOP_TIME);
-    printf("Time =%g\n", ffTime(GET_TIME));
-
+        /* -----------------        OUTPUT        ----------------- */
+        ffTime(START_TIME);
+        uintmax_t writePos = 0;
+        uint8_t Start, End = 0;
+        mutex writefileMutex;
+        ofstream outputFile(encodedFile, ios::binary | ios::out);
+        vector<uintmax_t> writePositions(NUM_OF_THREADS);
+        vector<uintmax_t> Starts(NUM_OF_THREADS);
+        vector<uintmax_t> Ends(NUM_OF_THREADS);
+        for (int i = 0; i < NUM_OF_THREADS; i++) {
+            Start = End;
+            Starts[i] = Start;
+            if (i == NUM_OF_THREADS - 1)
+                file[i] += (string(((file[i].size() - Start) % 8), '0'));
+            Ends[i] = End;
+            End = 8 - (file[i].size() - Start) % 8;
+            writePositions[i] = writePos;
+            writePos += ((file[i].size() - Start) + End);
+        }
+        FF_PARFOR_BEGIN(test3, i, 0, NUM_OF_THREADS, 1, 1, NUM_OF_THREADS) {
+            wWrite(Starts[i], Ends[i], &file, i, writePositions[i], &outputFile, &writefileMutex);
+        }FF_PARFOR_END(test3);
+        ffTime(STOP_TIME);
+        printf("Time =%g\n", ffTime(GET_TIME));
+    }
     return 0;
 }
