820dfb96a641489aa40316333dfad59b6ab3afd3
PaulMagos <paulmagos@icloud.com>
2023-08-26 17:33:00 +0200

    Update to Async

--- src/Async/Async.cpp
+++ src/Async/Async.cpp
@@ -20,10 +20,15 @@ using namespace std;
  * o decoded file path
  */
 #define OPT_LIST "hi:p:t:"
+#define ALL false
 
 void writeToFile(vector<string>* bits, const string& encodedFile);
 void readFrequencies(ifstream* myFile, uintmax_t len, vector<string>* file, vector<uintmax_t>* uAscii);
-void createOutput(vector<string>* file, const map<uintmax_t, string>& myMap);
+void encode(vector<string>* file, const map<uintmax_t, string>& myMap);
+void read(vector<string>* file, uintmax_t fileSize, ifstream* in);
+void count(vector<string>* file, vector<uintmax_t>* ascii);
+void toAscii(vector<string>* file, uintmax_t* writePos, vector<uintmax_t>* writePositions);
+void write(const string& encFile, vector<uintmax_t> writePositions, vector<string> file);
 
 int NUM_OF_THREADS = thread::hardware_concurrency();
 vector<future<void>> futures;
@@ -38,7 +43,9 @@ int main(int argc, char* argv[])
 
     MyDir = "./data/EncodedFiles/Async/";
     inputFile = "./data/TestFiles/";
-    csvFile = "./data/CSV/Async.csv";
+    csvFile = "./data/CSV/Async";
+    csvFile += (ALL? "All":"");
+    csvFile += ".csv";
 
     while((option = (char)getopt(argc, argv, OPT_LIST)) != -1){
         switch (option) {
@@ -60,39 +67,116 @@ int main(int argc, char* argv[])
 
     ifstream in(inputFile, ifstream::ate | ifstream::binary);
     uintmax_t fileSize = in.tellg();
-
-    vector<long> timers(5, 0);
     vector<string> file(NUM_OF_THREADS);
-    {
-        utimer timer("Total", &timers[4]);
+    vector<uintmax_t> writePositions = vector<uintmax_t>(NUM_OF_THREADS);
+    uintmax_t writePos = 0;
+
+    vector<long> timers((ALL?8:1), 0);
+    #if (ALL)
         {
-            utimer t("Read Frequencies", &timers[0]);
-            readFrequencies(&in, fileSize, &file, &ascii);
+        utimer timer("Total", &timers[6]);
+        {
+            utimer t("Read File", &timers[0]);
+            read(&file, fileSize, &in);
         }
         {
-            utimer t("Create Map", &timers[1]);
+            utimer t("Count Frequencies", &timers[1]);
+            count(&file, &ascii);
+        }
+        {
+            utimer t("Create Map", &timers[2]);
             Node::createMap(Node::buildTree(ascii), &myMap);
         }
         {
-            utimer t("Create Output", &timers[2]);
+            utimer t("Create Output", &timers[3]);
             createOutput(&file, myMap);
         }
         {
-            utimer t("Write To File", &timers[3]);
-            writeToFile(&file, encFile);
+            utimer t("Bits To Bytes", &timers[4]);
+            toAscii(file, &writePos, &writePositions);
+        }
+        {
+            utimer t("Write To File", &timers[5]);
+            ofstream outputFile(encFile, ios::binary | ios::out);
+            mutex writeFileMutex;
+            vector<std::thread> threads;
+            for (int i = 0; i < NUM_OF_THREADS; i++)
+                threads.emplace_back(writeFile, writePositions[i], file[i], &outputFile, &writeFileMutex);
+            for (int i = 0; i < NUM_OF_THREADS; i++) {
+                threads[i].join();
+            }
+
         }
     }
+    // Total without read and write
+    timers[7] = timers[1] + timers[2] + timers[3] + timers[4];
+    #else
+        read(&file, fileSize, &in);
+        {
+            utimer t("Total", &timers[0]);
+            count(&file, &ascii);
+            Node::createMap(Node::buildTree(ascii), &myMap);
+            encode(&file, myMap);
+            toAscii(&file, &writePos, &writePositions);
+//            readFrequencies(&in, fileSize, &file, &ascii);
+//            writeToFile(&file, encFile);
+        }
+        write(encFile, writePositions, file);
+        for (int i =0; i< NUM_OF_THREADS; i++) writePos += file[i].size();
+    #endif
 
-    string csv;
-    uintmax_t writePos = 0;
-    for (int i = 0; i < NUM_OF_THREADS; i++) writePos += file[i].size();
-    csv.append(encFileName).append(";");
-    csv.append(to_string(fileSize)).append(";");
-    csv.append(to_string(writePos/8)).append(";");
-    csv.append(to_string(NUM_OF_THREADS)).append(";");
-    for (long timer : timers) csv.append(to_string(timer)).append(";");
-    appendToCsv(csvFile, csv);
+    writeResults(encFileName, fileSize, writePos, NUM_OF_THREADS, timers, csvFile, false, ALL);
     return 0;
+}
+
+void read(vector<string>* file, uintmax_t fileSize, ifstream* in){
+    uintmax_t chunk = fileSize / NUM_OF_THREADS;
+    mutex readFileMutex;
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        chunk = (i == NUM_OF_THREADS-1)? fileSize - (chunk * (NUM_OF_THREADS-1)) : chunk;
+        futures.emplace_back(async(launch::async, readFile, &(*in), &(*file)[i], i*chunk, chunk, &readFileMutex));
+    }
+    for (int i = 0; i < NUM_OF_THREADS; i++) futures[i].get();
+}
+
+void count(vector<string>* file, vector<uintmax_t>* ascii){
+    futures = vector<future<void>>();
+    mutex asciiMutex;
+    for (int i = 0; i < NUM_OF_THREADS; i++)
+        futures.emplace_back(async(launch::async, countFrequency, &(*file)[i], &(*ascii), &asciiMutex));
+    for (int i = 0; i < NUM_OF_THREADS; i++) futures[i].get();
+}
+
+
+void toAscii(vector<string>* file, uintmax_t* writePos, vector<uintmax_t>* writePositions){
+    futures = vector<future<void>>();
+    vector<string> output(NUM_OF_THREADS);
+    uint8_t Start, End = 0;
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        Start = End;
+        if (i == NUM_OF_THREADS-1)
+            (*file)[i] += (string(8-((*file)[i].size()-Start)%8, '0'));
+        End = (8 - ((*file)[i].size()-Start)%8) % 8;
+        futures.emplace_back(async(launch::async, toByte, Start, End, (*file), i, &output[i]));
+        (*writePositions)[i] = (*writePos);
+        (*writePos) += (((*file)[i].size()-Start)+End);
+    }
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        futures[i].get();
+    }
+    cout << output[0] << endl;
+    (*file) = output;
+}
+
+void write(const string& encFile, vector<uintmax_t> writePositions, vector<string> file){
+    ofstream outputFile(encFile, ios::binary | ios::out);
+    mutex writeFileMutex;
+    vector<std::thread> threads;
+    for (int i = 0; i < NUM_OF_THREADS; i++)
+        threads.emplace_back(writeFile, writePositions[i], (file)[i], &outputFile, &writeFileMutex);
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        threads[i].join();
+    }
 }
 
 void readFrequencies(ifstream* myFile, uintmax_t len, vector<string>* file, vector<uintmax_t>* uAscii){
@@ -106,7 +190,7 @@ void readFrequencies(ifstream* myFile, uintmax_t len, vector<string>* file, vect
     for (int i = 0; i < NUM_OF_THREADS; i++) futures[i].get();
 }
 
-void createOutput(vector<string>* file, const map<uintmax_t, string>& myMap) {
+void encode(vector<string>* file, const map<uintmax_t, string>& myMap) {
     // Read file
     futures = vector<future<void>>();
     for (int i = 0; i < NUM_OF_THREADS; i++){
--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -11,6 +11,28 @@
 using namespace std;
 
 
+void readFile(ifstream* myFile, string* file, uintmax_t readPos, uintmax_t toRead, mutex* readFileMutex){
+    (*file) = string(toRead, ' ');
+    {
+        unique_lock<mutex> lock(*readFileMutex);
+        (*myFile).seekg(readPos);
+        (*myFile).read(&(*file)[0], toRead);
+    }
+}
+
+void countFrequency(string* file, vector<uintmax_t>* uAscii, mutex* writeAsciiMutex){
+    vector<uintmax_t> ascii(ASCII_MAX, 0);
+    for (char j : (*file)) (ascii)[j]++;
+    {
+        unique_lock<mutex> lock(*writeAsciiMutex);
+        for (int j = 0; j < ASCII_MAX; j++) {
+            if ((ascii)[j] != 0) {
+                (*uAscii)[j] += (ascii)[j];
+            }
+        }
+    }
+}
+
 /*
  * Read the file and count the frequencies of each character
  * @param myFile: the file to read
@@ -45,12 +67,40 @@ void calcChar(ifstream* myFile, vector<string>* file, int i, int nw, uintmax_t l
     }
 }
 
-void toBits(map<uintmax_t, string> myMap, vector<string>* line, int i){
+void toBits(map<uintmax_t, string> myMap, vector<string>* file, int i){
     string bits;
-    for (char j : (*line)[i]) {
+    for (char j : (*file)[i]) {
         bits.append(myMap[j]);
     }
-    (*line)[i] = bits;
+    (*file)[i] = bits;
+}
+
+void toByte(uint8_t Start, uint8_t End, vector<string> bits, int pos, string* out){
+    string output;
+    uint8_t value = 0;
+    int i;
+    for (i = Start; i < (bits)[pos].size(); i+=8) {
+        for (uint8_t n = 0; n < 8; n++)
+            value = ((bits)[pos][i+n]=='1') | value << 1;
+        output.append((char*) (&value), 1);
+        value = 0;
+    }
+    if (pos != (bits).size()-1) {
+        for (uint32_t n = 0; n < 8-End; n++)
+            value = ((bits)[pos][i-8+n]=='1') | value << 1;
+        for (i = 0; i < End; i++)
+            value = ((bits)[pos+1][i]=='1') | value << 1;
+        output.append((char*) (&value), 1);
+    }
+    (*out)=output;
+}
+
+void writeFile(uintmax_t writePos, const string& output, ofstream* outputFile, mutex* fileMutex){
+    {
+        unique_lock<mutex> lock((*fileMutex));
+        (*outputFile).seekp(writePos/8);
+        (*outputFile).write((output).c_str(), (output).size());
+    }
 }
 
 void wWrite(uint8_t Start, uint8_t End, vector<string>* bits, int pos, uintmax_t writePos, ofstream* outputFile, mutex* fileMutex){
@@ -77,18 +127,25 @@ void wWrite(uint8_t Start, uint8_t End, vector<string>* bits, int pos, uintmax_t
     }
 }
 
-void appendToCsv(const string& filename, const string& data, bool pool = false){
+
+void writeResults(const string& testName, uintmax_t fileSize, uintmax_t writePos, int numOfThreads, const vector<long>& timers, const string& csvFile, bool pool= false, bool all=false){
+    string csv;
+    csv.append(testName).append(";");
+    csv.append(to_string(fileSize)).append(";");
+    csv.append(to_string(writePos/8)).append(";");
+    csv.append(to_string(numOfThreads)).append(";");
+    for (long timer : timers) csv.append(to_string(timer)).append(";");
     ofstream file;
     // Open file, if empty add header else append
-    file.open(filename, ios::out | ios::app);
-    if (file.tellp() == 0)
-        if (pool)
-            file << "Test File;File Size;Encoding Size;Nw;Tasks;Start Pool;Read;Tree;Encode;Write;End Pool;Total" << endl;
-        else
-            file << "Test File;File Size;Encoding Size;Nw;Read;Tree;Encode;Write;Total" << endl;
+    file.open(csvFile, ios::out | ios::app);
+    if (file.tellp() == 0) {
+        file << "Test File;File Size;Encoding Size;Nw;" << (pool? "Tasks;":"");
+        if (all) file << (pool? "Start Pool;":"") << "Read;Tree;Encode;Write;" << (pool? "End Pool;":"");
+        file << "Total;" << endl;
+    }
     else
         file.seekp(-1, ios_base::end);
-    file << data << endl;
+    file << csv << endl;
     file.close();
 }
 
